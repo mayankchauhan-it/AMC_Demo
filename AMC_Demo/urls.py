c3ebe419414d6f9e3bc5b61dac22f2956b642b92
mayank-quantumbot <mayankchauhan.it@gmail.com>
2023-08-04 15:25:26 +0530

    Live Testing

--- AMC_Demo/urls.py
+++ AMC_Demo/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('admin/', include('admin_panel.urls')),
+    path('', include('admin_panel.urls')),
 ]
 
 
