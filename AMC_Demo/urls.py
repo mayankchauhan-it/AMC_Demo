94242bae77ace0da3755ef8c5b70fd146f89980d
mayank-quantumbot <mayankchauhan.it@gmail.com>
2023-08-04 15:43:49 +0530

    Live Testing........................

--- AMC_Demo/urls.py
+++ AMC_Demo/urls.py
@@ -20,5 +20,5 @@ from django.conf import settings
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('https://amc-demo.vercel.app', include('admin_panel.urls')),
+    path('', include('admin_panel.urls')),
 ]
